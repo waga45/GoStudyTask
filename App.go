b8837a4b4dfa58b8c9f9caea7d173e935736b943
fplei <1553234169@qq.com>
2025-08-27 22:52:52 +0800

    task1

--- App.go
+++ App.go
@@ -17,7 +17,25 @@ func main() {
 	ishw := task1.IsHwNumber(testNum)
 	fmt.Println("数字", testNum, " 是否为回文:", ishw)
 
+	var ln = "([{}])"
+	ln = "[()]"
+	b := task1.EnableParticularlyStr(ln)
+	fmt.Println("是否有效字符串:", b)
+
 	var ss = []string{"flat", "flower", "fliter"}
 	res := task1.FrequentlyMaxChar(&ss)
 	fmt.Println("最长公共前缀：", res)
+
+	var dd = []int{1, 2, 3, 6}
+	dd_res := task1.DigitsAddition(dd)
+	fmt.Println("值+1 结果：", dd_res)
+
+	task1.RemoveRepeatItem([]int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4})
+
+	intervals := [][]int{{1, 3}, {2, 6}, {8, 10}, {15, 18}}
+	task1.RemoveRepeatDemension(intervals)
+
+	nums := []int{2, 7, 11, 15}
+	indexResult := task1.CaculateToTarget(nums, 17)
+	fmt.Println(indexResult)
 }
--- task1/WorkA.go
+++ task1/WorkA.go
@@ -2,6 +2,7 @@ package task1
 
 import (
 	"errors"
+	"fmt"
 	"strconv"
 	"strings"
 )
@@ -105,6 +106,76 @@ func reverseArray(ss []rune) []rune {
 	return swapArr
 }
 
+/*
+*
+
+	给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效
+*/
+func EnableParticularlyStr(input string) bool {
+	if (len(input) <= 1) || (len(input)%2 != 0) {
+		return false
+	}
+	if len(input) == 2 {
+		return charCompare(string(input[0]), string(input[1]))
+	}
+	inputArr := []rune(input)
+	var itemCount = findNoRepeatLen(&inputArr)
+	var trueCount = 0
+	loopFind(&inputArr, &trueCount)
+	return trueCount == itemCount
+}
+func loopFind(arr *[]rune, trueCount *int) {
+	for i := 0; i < len(*arr); i++ {
+		fmt.Println("loop start :", *arr)
+		var cr = string((*arr)[i])
+		var cn string
+		if (i + 1) < len(*arr) {
+			cn = string((*arr)[i+1])
+		} else {
+			cn = string((*arr)[i])
+		}
+		b := charCompare(cr, cn)
+		if b == true {
+			*arr = append((*arr)[:i], (*arr)[i+2:]...)
+			*trueCount += 1
+			loopFind(arr, trueCount)
+		}
+	}
+}
+func findNoRepeatLen(arr *[]rune) int {
+	var tempMap = map[string]int{}
+	for _, v := range *arr {
+		_, flag := tempMap[string(v)]
+		if !flag {
+			tempMap[string(v)] = 1
+		}
+	}
+	delete(tempMap, ")")
+	delete(tempMap, "}")
+	delete(tempMap, "]")
+	return len(tempMap)
+}
+func charCompare(c1 string, c2 string) bool {
+	switch c1 {
+	case "(":
+		if c2 == ")" {
+			return true
+		}
+	case "{":
+		if c2 == "}" {
+			return true
+		}
+	case "[":
+		if c2 == "]" {
+			return true
+		}
+	}
+	return false
+}
+
+/*
+计算最长公共长缀
+*/
 func FrequentlyMaxChar(content *[]string) string {
 	if len(*content) <= 0 {
 		return ""
@@ -131,4 +202,98 @@ func FrequentlyMaxChar(content *[]string) string {
 		}
 	}
 	return strings.Join(maxArray, "")
+}
+
+/*
+*
+数+1
+*/
+func DigitsAddition(nums []int) []int {
+	if len(nums) <= 0 {
+		return nums
+	}
+	ss := make([]string, len(nums))
+	for i, v := range nums {
+		ss[i] = strconv.Itoa(v)
+	}
+	num, _ := strconv.Atoi(strings.Join(ss, ""))
+	num += 1
+
+	var resultArr = []int{}
+	for _, v := range strconv.Itoa(num) {
+		fmt.Println(v)
+		n, _ := strconv.Atoi(string(v))
+		resultArr = append(resultArr, n)
+	}
+	return resultArr
+}
+
+/*
+*
+移除重复
+*/
+func RemoveRepeatItem(arr []int) (int, []int) {
+	if len(arr) <= 1 {
+		return len(arr), arr
+	}
+	slow := 0
+	// 4,1,2,2,3,4,2
+	for i := 1; i < len(arr); i++ {
+		if (arr)[i] != (arr)[slow] {
+			slow++
+			arr[slow] = arr[i]
+		}
+	}
+	fmt.Println(arr[:slow+1])
+	return slow + 1, arr[:slow+1]
+}
+
+/*
+* 56.合并区间
+ */
+func RemoveRepeatDemension(list [][]int) [][]int {
+	if len(list) <= 1 {
+		return list
+	}
+	fmt.Println("输入：", list)
+	var resultArr = [][]int{}
+	for rowIndex := 0; rowIndex < len(list); rowIndex++ {
+		one := list[rowIndex]
+		onceMax := one[1]
+		for j := rowIndex + 1; j < len(list); j++ {
+			next := list[j]
+			if one[1]-next[0] >= 0 {
+				onceMax = next[1]
+				rowIndex = j
+			}
+		}
+		resultArr = append(resultArr, []int{one[0], onceMax})
+	}
+	fmt.Println("合并区间结果：", resultArr)
+	return resultArr
+}
+
+/*
+*两数之和
+ */
+func CaculateToTarget(arr []int, target int) []int {
+	if len(arr) <= 0 {
+		return nil
+	}
+	for i := 0; i < len(arr); i++ {
+		var c = arr[i]
+		find := false
+		findRightIndex := 0
+		for j := i + 1; j < len(arr); j++ {
+			if (c + arr[j]) == target {
+				find = true
+				findRightIndex = j
+				break
+			}
+		}
+		if find {
+			return []int{i, findRightIndex}
+		}
+	}
+	return nil
 }
